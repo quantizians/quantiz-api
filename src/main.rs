d1f493b8c681d7b4b3205c49a9818dfb83e42993
vnphanquang-x1-ubuntu <contact.quangphan@gmail.com>
2020-03-28 20:40:38 +0700

    changes dev url schema, init tests module, abstract rocket initiation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+#[cfg(test)] pub mod tests;
+
 #[macro_use] extern crate rocket;
+use rocket::Rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     return "Quantiz index";
 }
 
+fn rocket() -> Rocket {
+    return rocket::ignite().mount("/", routes![index]);   
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
