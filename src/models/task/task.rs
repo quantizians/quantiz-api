630a15bba3a6a8052300dbda010a9074421c0fa0
vnphanquang-x1-ubuntu <contact.quangphan@gmail.com>
2020-04-18 22:27:25 +0700

    feat(Task):  implement cross-comparison between Task & OptionalizedTask

--- src/models/task/task.rs
+++ src/models/task/task.rs
@@ -1,11 +1,14 @@
-#![allow(clippy::redundant_static_lifetimes)]
+#![allow(
+  clippy::redundant_static_lifetimes, 
+  clippy::needless_return,
+)]
 use chrono::NaiveDateTime;
 use uuid::Uuid;
 use serde::{Serialize, Deserialize};
 use crate::db::schema::tasks;
 use super::Priority;
 
-#[derive(Queryable, Identifiable, Debug, Clone, Serialize, Deserialize)]
+#[derive(Queryable, Identifiable, Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[table_name="tasks"]
 pub struct Task {
   pub id: Uuid,
@@ -21,12 +24,12 @@ pub struct Task {
   pub supertask: Option<Uuid>,
 }
 
-#[derive(Insertable, AsChangeset, Debug, Clone, Serialize, Deserialize)]
+#[derive(Insertable, AsChangeset, Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[table_name="tasks"]
-pub struct OptionalizedTask<'a> {
+pub struct OptionalizedTask {
   pub id: Option<Uuid>,
-  pub title: Option<&'a str>,
-  pub details: Option<&'a str>,
+  pub title: Option<String>,
+  pub details: Option<String>,
   #[serde(with = "nullable_iso_timestamp")]
   #[serde(default)]
   pub created_date: Option<NaiveDateTime>,
@@ -37,6 +40,36 @@ pub struct OptionalizedTask<'a> {
   pub persistent: Option<bool>,
   pub completed: Option<bool>,
   pub supertask: Option<Uuid>,
+}
+
+impl From<Task> for OptionalizedTask {
+  fn from(task: Task) -> Self {
+    return OptionalizedTask {
+      id: Some(task.id),
+      title: Some(task.title),
+      details: task.details,
+      created_date: Some(task.created_date),
+      deadline: task.deadline,
+      priority: Some(task.priority),
+      persistent: Some(task.persistent),
+      completed: Some(task.completed),
+      supertask: task.supertask,
+    };
+  }
+}
+
+impl PartialEq<OptionalizedTask> for Task {
+  fn eq(&self, other: &OptionalizedTask) -> bool {
+    let optionalized_self = OptionalizedTask::from(self.clone());
+    return &optionalized_self == other;
+  }
+}
+
+impl PartialEq<Task> for OptionalizedTask {
+  fn eq(&self, other: &Task) -> bool {
+    let optionalized_other = OptionalizedTask::from(other.clone());
+    return &optionalized_other == self;
+  }
 }
 
 mod iso_timestamp {
