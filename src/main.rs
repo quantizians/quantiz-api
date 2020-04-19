92b453d8bdfbb4aff3cf83ce31c50ef761513c45
Quang Phan <contact.quangphan@gmail.com>
2020-04-19 16:41:18 +0700

    chore(ci): set up github action for ci (#1)
    * chore(ci): set up github action for ci
    
    * chore(ci): workflow name & badge

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[cfg(test)] pub mod tests;
 #[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_migrations;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 use rocket::Rocket;
+use rocket::fairing::AdHoc;
 use dotenv::dotenv;
 // #[macro_use] extern crate dotenv_codegen;
 // use dotenv_codegen::dotenv;
@@ -17,11 +19,27 @@ mod routers;
 mod responses;
 use db::DbConnection;
 
+// This macro from `diesel_migrations` defines an `embedded_migrations` module
+// containing a function named `run`. This allows the example to be run and
+// tested without any outside setup of the database.
+embed_migrations!();
+
+fn run_db_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
+    let conn = DbConnection::get_one(&rocket).expect("invalid database connection");
+    match diesel_migrations::run_pending_migrations(&*conn) {
+        Ok(()) => Ok(rocket),
+        Err(e) => {
+            panic!("Failed to run database migrations: {}", e.to_string());
+        }
+    }
+}
+
 fn rocket() -> Rocket {
     let api_version = std::env::var("API_VERSION").expect("invalid API version");
     let server_root = format!("/api/{}", api_version);
     return rocket::ignite()
         .attach(DbConnection::fairing())
+        .attach(AdHoc::on_attach("Database Migrations", run_db_migrations))
         .mount(
             &routers::tasks::root(&server_root), 
             routers::tasks::handlers()
